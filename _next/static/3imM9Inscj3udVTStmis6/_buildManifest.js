27821ff2cfd5365f39638ce64c7ed0925d8c41e9
koh110 <koh110@users.noreply.github.com>
2022-03-18 02:03:33 +0000

    deploy: 822ed4fd3cac07feb1efc49e84cf58d577fd38c0

--- _next/static/3imM9Inscj3udVTStmis6/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/43ea133b-235492eb8aa22e81.js","static/chunks/597-3fb68d97794784b7.js","static/chunks/pages/index-40b83ce894d41909.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
