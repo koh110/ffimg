773a17f63bf89fb758248de751b2a8e9061d2da2
koh110 <koh110@users.noreply.github.com>
2023-03-01 10:55:18 +0000

    deploy: 3715cccd93ccab79c81c766b773f9bb8d76ac9be

---
+++ _next/static/NHN_LRP1bo7PGXoU8bSkb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/43ea133b-414cbff27a4aabc0.js","static/chunks/769-5bdf0475262e049c.js","static/chunks/pages/index-acc55fa34f83c3a6.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
